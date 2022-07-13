4174a21ff4dac9c38a9b150e8243796794049bdb
Stefania <tudorica.stefania@gmail.com>
2022-07-13 19:24:22 +0300

    Added Author & Title

--- src/main/java/ro/sda/javaremote37/springseed/controller/BookRestController.java
+++ src/main/java/ro/sda/javaremote37/springseed/controller/BookRestController.java
@@ -1,10 +1,10 @@
 package ro.sda.javaremote37.springseed.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import ro.sda.javaremote37.springseed.dto.BookDto;
 import ro.sda.javaremote37.springseed.entities.Book;
+import ro.sda.javaremote37.springseed.services.BookService;
 
 import java.util.List;
 
@@ -12,8 +12,16 @@ import java.util.List;
 @RequestMapping("/api/book")
 public class BookRestController {
 
+    @Autowired
+    private BookService bookService;
     @GetMapping
     public List<BookDto> getAllBooks(){
+        return bookService.getAll();
 
+    }
+
+    @PostMapping
+    public BookDto createBook(@RequestBody BookDto bookDto){
+        return bookService.createBook(bookDto);
     }
 }
--- src/main/java/ro/sda/javaremote37/springseed/services/BookService.java
+++ src/main/java/ro/sda/javaremote37/springseed/services/BookService.java
@@ -1,10 +1,30 @@
 package ro.sda.javaremote37.springseed.services;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ro.sda.javaremote37.springseed.dto.BookDto;
+import ro.sda.javaremote37.springseed.entities.Book;
 import ro.sda.javaremote37.springseed.repository.BookRepository;
 
-@Service
-public class BookService {
-    private BookRepository bookRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@AllArgsConstructor
+public class BookService {
+
+    private BookRepository bookRepository;
+    private BookMapper bookMapper;
+
+    public List<BookDto> getAll() {
+       return bookRepository.findAll().stream().map(e->bookMapper.convertToDto(e)).collect(Collectors.toList());
+    }
+
+    public BookDto createBook(BookDto bookDto) {
+        Book entity = bookMapper.convertToEntity(bookDto);
+        entity= bookRepository.save(entity);
+        BookDto result = bookMapper.convertToDto(entity);
+        return result;
+    }
 }
